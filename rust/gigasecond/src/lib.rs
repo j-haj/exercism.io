3e90d2c402a57e241779e635e20925fa396f1b96
Jeff Hajewski <jeff.hajewski@gmail.com>
2018-11-13 13:21:13 -0600

    Finishes gigasecond exercise in Rust track.

---
+++ rust/gigasecond/src/lib.rs
@@ -0,0 +1,7 @@
+extern crate chrono;
+use chrono::{DateTime, Duration, Utc};
+
+// Returns a Utc DateTime one billion seconds after start.
+pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
+    start.checked_add_signed(Duration::seconds(1_000_000_000)).expect("Overflow")
+}
