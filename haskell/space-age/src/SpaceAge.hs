1bf26f45e1d7e872a20d262c70984872ae4a9f60
Jeff Hajewski <jeff.hajewski@gmail.com>
2018-11-20 05:10:17 -0600

    Refactor SpaceAge solution from Haskell track.

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -10,13 +10,16 @@ data Planet = Mercury
             | Neptune
 
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds = case planet of Mercury -> earthYears / 0.2408467
-                                      Venus -> earthYears / 0.61519726
-                                      Earth -> earthYears
-                                      Mars -> earthYears / 1.8808158
-                                      Jupiter -> earthYears / 11.862615
-                                      Saturn -> earthYears / 29.447498
-                                      Uranus -> earthYears / 84.016846
-                                      Neptune -> earthYears / 164.79132
-  where earthYears = seconds / 31557600.0
+ageOn planet seconds = let orbitalPeriod = case planet of
+                             Mercury -> 0.2408467
+                             Venus -> 0.61519726
+                             Earth -> 1.0
+                             Mars -> 1.8808158
+                             Jupiter -> 11.862615
+                             Saturn ->  29.447498
+                             Uranus ->  84.016846
+                             Neptune -> 164.79132
+                           earthYears = seconds / 31557600.0
+                       in earthYears / orbitalPeriod
+
 
