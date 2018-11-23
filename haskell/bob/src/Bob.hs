18708b4dfb344cbce724037771ee2ee3c8fa56b0
Jeff Hajewski <jeff.hajewski@gmail.com>
2018-11-23 08:01:11 -0600

    Finish Bob exercise in Haskell track.

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,11 +1,29 @@
 module Bob (responseFor) where
-import Data.Char (isUpper)
+import Data.Char (isUpper, isLetter, isSpace)
+import Data.String.Utils 
 
 isQuestion :: String -> Bool
-isQuestion s = '?' `elem` s
+isQuestion s = '?' == (last . strip) s
+
+allLettersAreCaps :: String -> Bool
+allLettersAreCaps s = foldr (\x acc -> acc && if isLetter x
+                                              then isUpper x
+                                              else True)
+                      True s
+
+hasLetters :: String -> Bool
+hasLetters s = foldl (\acc x -> acc || isLetter x) False s
 
 isYelled :: String -> Bool
-isYelled s = foldr (\x acc -> acc && isUpper x) s
+isYelled s = allLettersAreCaps s && hasLetters s
+
+isAllWhitespace :: String -> Bool
+isAllWhitespace s = foldr (\x acc -> acc && isSpace x) True s
 
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor xs
+  | null xs || isAllWhitespace xs = "Fine. Be that way!"
+  | isQuestion xs && isYelled xs = "Calm down, I know what I'm doing!"
+  | isQuestion xs = "Sure."
+  | isYelled xs = "Whoa, chill out!"
+  | otherwise = "Whatever."
