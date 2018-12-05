4aefe8c4bafbb69199fe84296df40dfd75ddcc23
Jeff Hajewski <jeff.hajewski@gmail.com>
2018-12-05 09:05:24 -0600

    Abstract repeated lambdas to helper functions.

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -2,23 +2,26 @@ module Bob (responseFor) where
 import Data.Char (isUpper, isLetter, isSpace)
 import Data.String.Utils 
 
+nLetters :: String -> Int
+nLetters s = length $ filter (isLetter) s
+
+nUppers :: String -> Int
+nUppers s = length $ filter (isUpper) s
+
 isQuestion :: String -> Bool
 isQuestion s = '?' == (last . strip) s
 
 allLettersAreCaps :: String -> Bool
-allLettersAreCaps s = foldr (\x acc -> acc && if isLetter x
-                                              then isUpper x
-                                              else True)
-                      True s
+allLettersAreCaps s = (nLetters s) == (nUppers s)
 
 hasLetters :: String -> Bool
-hasLetters s = foldl (\acc x -> acc || isLetter x) False s
+hasLetters s = (nLetters s) > 0
 
 isYelled :: String -> Bool
 isYelled s = allLettersAreCaps s && hasLetters s
 
 isAllWhitespace :: String -> Bool
-isAllWhitespace s = foldr (\x acc -> acc && isSpace x) True s
+isAllWhitespace s = (length $ filter (isSpace) s) == (length s)
 
 responseFor :: String -> String
 responseFor xs
