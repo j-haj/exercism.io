ab6b79b392b1f4fdbaec99e9545c9a94b3c3960f
Jeff Hajewski <jeff.hajewski@gmail.com>
2018-12-11 05:34:31 -0600

    Simplify Bob solution

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -2,30 +2,16 @@ module Bob (responseFor) where
 import Data.Char (isUpper, isLetter, isSpace)
 import Data.String.Utils 
 
-nLetters :: String -> Int
-nLetters s = length $ filter (isLetter) s
-
-nUppers :: String -> Int
-nUppers s = length $ filter (isUpper) s
-
 isQuestion :: String -> Bool
 isQuestion s = '?' == (last . strip) s
 
-allLettersAreCaps :: String -> Bool
-allLettersAreCaps s = (nLetters s) == (nUppers s)
-
-hasLetters :: String -> Bool
-hasLetters s = (nLetters s) > 0
-
 isYelled :: String -> Bool
-isYelled s = allLettersAreCaps s && hasLetters s
-
-isAllWhitespace :: String -> Bool
-isAllWhitespace s = (length $ filter (isSpace) s) == (length s)
+isYelled s = let normS = filter (isLetter) s in
+  length normS == (length $ filter (isUpper) normS) && length normS > 0
 
 responseFor :: String -> String
 responseFor xs
-  | null xs || isAllWhitespace xs = "Fine. Be that way!"
+  | null $ filter (not . isSpace) xs = "Fine. Be that way!"
   | isQuestion xs && isYelled xs = "Calm down, I know what I'm doing!"
   | isQuestion xs = "Sure."
   | isYelled xs = "Whoa, chill out!"
