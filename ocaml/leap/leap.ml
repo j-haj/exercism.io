4df9d6bac49d4ba1250d799959fc175eff68d436
Jeff Hajewski <jeff.hajewski@gmail.com>
2019-02-04 05:47:04 -0600

    Update OCaml leap solution.

--- ocaml/leap/leap.ml
+++ ocaml/leap/leap.ml
@@ -1,11 +1,8 @@
 let leap_year year =
-  let is_mod_4 = year mod 4 == 0 in
-  let is_mod_100 = year mod 100 == 0 in
-  let is_mod_400 = year mod 400 == 0 in
   match year with
-    _ when is_mod_4 && is_mod_100 && is_mod_400 -> true
-  | _ when is_mod_4 && is_mod_100 -> false
-  | _ when is_mod_4 -> true
+    _ when year mod 400 = 0 -> true
+  | _ when year mod 100 = 0 -> false
+  | _ when year mod 4 = 0 -> true
   | _ -> false
                          
              
