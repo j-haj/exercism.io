b7c7938a29f1ddaef65d13da65467d3bdb8d7fb3
j-haj <jeff.hajewski@gmail.com>
2019-02-26 07:00:53 -0600

    Adds nth prime exercise.

---
+++ haskell/nth-prime/src/Prime.hs
@@ -0,0 +1,15 @@
+module Prime (nth) where
+
+import Data.Set (Set)
+
+isPrime :: Int -> Bool
+isPrime n
+  | n % 2 == 0 || n < 2 = False
+  | otherwise = primeSearcher n 3
+  where primeSearcher n i = if n % i == 0 then False
+                            else if n <= i then True
+                                 else primeSearcher n (i + 2)
+  
+
+nth :: Int -> Maybe Integer
+nth n = error "You need to implement this function."
