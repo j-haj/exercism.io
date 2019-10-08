3322d4409f230bbb85e894e4f13bec61252f1987
j-haj <jeff.hajewski@gmail.com>
2019-10-08 09:25:17 -0500

    Adds initial work on house problem from scala track.

---
+++ scala/house/src/main/scala/House.scala
@@ -0,0 +1,21 @@
+object House {
+  private val phrases = Vector(
+    ("the malt", "lay"),
+    ("rat", "ate"),
+    ("cat", "killed"),
+    ("dog", "worried"),
+    ("cow with the crumpled horn", "tossed"),
+    ("maiden all forlorn", "milked"),
+    ("man all tattered and torn", "kissed"),
+    ("priest all shaven and shorn", "married"),
+    ("rooster that crowed in the morn", "woke"),
+    ("farmer sowing his corn", "kept"),
+    ("horse and the hound and the horn", "belonged")
+  )
+
+  def recite(start: Int, end: Int): String = {
+    def loop(n: Int, acc: StringBuilder): String = {
+      
+    }
+  }
+}
